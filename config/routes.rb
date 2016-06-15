6c2f045b90b21be097e06fdf2a75681ac6f9450b
JamesWinestock <james.winestock@gmail.com>
2016-06-15 14:43:45 +1000

    chats start

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :index, :show]
   resources :profiles
+  resources :chats, only: [:new]
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615015424) do
+ActiveRecord::Schema.define(version: 20160615044249) do
+
+  create_table "chats", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
